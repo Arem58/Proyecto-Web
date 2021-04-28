413186025661be1dc2fbfb7fc9c466e615113297
Arem58 <arem.9958@gmail.com>
2021-04-28 14:30:54 -0600

    build terminado

--- src/componentes/BuildNav/Build.jsx
+++ src/componentes/BuildNav/Build.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react'
 import '../../styles/Build.css'
 import PC from '../../Recursos/pcGrey.png'
@@ -5,34 +7,124 @@ import Xbox from '../../Recursos/xb1.png'
 import PS4 from '../../Recursos/ps4.png'
 import Switch from '../../Recursos/switch.png'
 
-const Build = () => (
-  <div className="mainContainer">
-    <div className="buildsContainer">
-      <div className="aContainer">
-        <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
-          <img className="imgSize" src={PC} alt="Imagen de pc" />
-        </a>
+const Build = () => {
+  const [builsPos, setbuilsPos] = React.useState(0)
+  const stylePC = {
+    color: '#212121',
+  }
+  const styleXbox = {
+    color: '#005400',
+  }
+  const stylePS4 = {
+    color: '#034169',
+  }
+  const styleSwitch = {
+    color: '#d30000',
+  }
+  const barraPC = {
+    backgroundColor: '#212121',
+  }
+  const barraXbox = {
+    backgroundColor: '#005400',
+  }
+  const barraPS4 = {
+    backgroundColor: '#034169',
+  }
+  const barraSwitch = {
+    backgroundColor: '#d30000',
+  }
+  const position1 = {
+    left: '30px',
+  }
+  const position2 = {
+    left: '20px',
+  }
+  const position3 = {
+    left: '40px',
+  }
+  const posicion = {
+    transform: `translate(${builsPos}px`,
+  }
+  const handleKeyDown = (event) => {
+    event.preventDefault()
+    if (builsPos === 0) {
+      setbuilsPos(-94)
+    } else {
+      setbuilsPos(0)
+    }
+  }
+  const text = 'Prime Vault: \nZephyr & \nChroma'
+  return (
+    <div style={posicion} className="mainContainer">
+      <div className="buildsContainer">
+        <div>
+          <div className="aContainer">
+            <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
+              <img className="imgSize" src={PC} alt="Imagen de pc" />
+            </a>
+          </div>
+          <div style={stylePC} className="textDesing">{text}</div>
+          <div className="containBar">
+            <div className="barra" style={barraPC} />
+            <div className="divisor" style={position1} />
+          </div>
+          <div style={stylePC} className="textDesing">Live</div>
+        </div>
+        <hr />
+        <div>
+          <div className="aContainer">
+            <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
+              <img className="imgSize" src={Xbox} alt="Imagen de pc" />
+            </a>
+          </div>
+          <div style={styleXbox} className="textDesing">{text}</div>
+          <div className="containBar">
+            <div className="barra" style={barraXbox} />
+            <div className="divisor" style={position2} />
+            <div className="divisor" style={position3} />
+          </div>
+          <div style={styleXbox} className="textDesing">Live</div>
+        </div>
+        <hr />
+        <div>
+          <div className="aContainer">
+            <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
+              <img className="imgSize" src={PS4} alt="Imagen de pc" />
+            </a>
+          </div>
+          <div style={stylePS4} className="textDesing">{text}</div>
+          <div className="containBar">
+            <div className="barra" style={barraPS4} />
+            <div className="divisor" style={position2} />
+            <div className="divisor" style={position3} />
+          </div>
+          <div style={stylePS4} className="textDesing">Live</div>
+        </div>
+        <hr />
+        <div>
+          <div className="aContainer">
+            <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
+              <img className="imgSize" src={Switch} alt="Imagen de pc" />
+            </a>
+          </div>
+          <div style={styleSwitch} className="textDesing">{text}</div>
+          <div className="containBar">
+            <div className="barra" style={barraSwitch} />
+            <div className="divisor" style={position2} />
+            <div className="divisor" style={position3} />
+          </div>
+          <div style={styleSwitch} className="textDesing">Live</div>
+        </div>
       </div>
-      <hr />
-      <div className="aContainer">
-        <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
-          <img className="imgSize" src={Xbox} alt="Imagen de pc" />
-        </a>
-      </div>
-      <hr />
-      <div className="aContainer">
-        <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
-          <img className="imgSize" src={PS4} alt="Imagen de pc" />
-        </a>
-      </div>
-      <hr />
-      <div className="aContainer">
-        <a className="aSize" href="https://www3.animeflv.net/anime/mushoku-tensei-isekai-ittara-honki-dasu">
-          <img className="imgSize" src={Switch} alt="Imagen de pc" />
-        </a>
+      <div className="interactuable" onClick={handleKeyDown} role="button">
+        <div className="triangle1" />
+        <div className="containerInteractuable">
+          <p className="styleP">Warframe Builds</p>
+        </div>
+        <div className="triangle2" />
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Build
